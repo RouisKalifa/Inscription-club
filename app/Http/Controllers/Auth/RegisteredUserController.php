ed732432cda95b50d1bcf4ede549202b76e10535
kalifarouis <kalifarouis@Kalifa>
2025-06-08 15:36:22 +0200

    rectification erreur register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,10 +36,14 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    'name'     => $request->name,
+    'email'    => $request->email,
+    'password' => Hash::make($request->password),
+    // On force ici le rôle "secretaire" :
+    'role'     => 'secretaire',
+]);
+
+
 
         event(new Registered($user));
 
